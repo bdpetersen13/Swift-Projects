b30b6832ccc4f3d7020a358416f26a0fd2c84d78
Brandon Petersen <89234922+bdpetersen13@users.noreply.github.com>
2023-04-30 15:42:24 -0400

    Building Clipper Application

---
+++ Clipper/Clipper/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Clipper
+//
+//  Created by Brandon Petersen on 4/30/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func eraseClipboard(_ sender: UIButton) {
+        let pasteboard = UIPasteboard.general
+            pasteboard.string = ""
+            clipboardLabel.isHidden = false
+            
+            let alertController = UIAlertController(title: "Clipboard Erased!", message: nil, preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alertController.addAction(okAction)
+            present(alertController, animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        clipboardLabel.isHidden = true
+        addButtonBorder()
+    }
+
+    @IBOutlet weak var eraseButton: UIButton!
+    @IBOutlet weak var clipboardLabel: UILabel!
+    
+
+    private func addButtonBorder() {
+        eraseButton.layer.borderWidth = 2.0
+        eraseButton.layer.cornerRadius = 36.0
+        eraseButton.layer.masksToBounds = true
+    }
+}
+
